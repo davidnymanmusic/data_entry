1dcc5adacb4cb0a191a592b809e4aa8420b67da4
David Nyman <davenyman@gmail.com>
2018-07-26 16:15:34 -0400

    initial commit

---
+++ src/NavBar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+const NavComponent = () =>
+			<nav>
+				<div className="navWide">
+					<div className="wideDiv">
+						<a href="#">Link 1</a>
+						<a href="#">Link 2</a>
+						<a href="#">Link 3</a>
+					</div>
+				</div>
+			</nav>
+
+
+// 	burgerToggle: function() {
+// 		let linksEl = document.querySelector('.narrowLinks');
+// 		if (linksEl.style.display === 'block') {
+// 			linksEl.style.display = 'none';
+// 		} else {
+// 			linksEl.style.display = 'block';
+// 		}
+// 	}
+// });
+
+export default NavComponent
