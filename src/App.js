76478f329c73fda2b79b723ebe2c036872132075
David Nyman <davenyman@gmail.com>
2018-07-27 10:44:45 -0400

    clean up code

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 import PostForm from './PostForm';
 import AllPost from './AllPost';
 
-
 class App extends Component {
-render() {
-return (
-<div className="App">
-	<h1 id="logo">Skinno.</h1>
-  {/* <div className="navbar">
+	render() {
+		return (
+			<div className="App">
+				<h1 id="logo">Skinno.</h1>
+				{/* <div className="navbar">
     <h2 className="center ">Data Entry</h2>
     </div> */}
-    <PostForm />
-    <AllPost />
-</div>
-);
-}
+				<PostForm />
+				<AllPost />
+			</div>
+		);
+	}
 }
 export default App;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 
-const NavComponent = () =>
-			<nav>
-				<div className="navWide">
-					<div className="wideDiv">
-						<a href="#">Link 1</a>
-						<a href="#">Link 2</a>
-						<a href="#">Link 3</a>
-					</div>
-				</div>
-			</nav>
-
+const NavComponent = () => (
+	<nav>
+		<div className="navWide">
+			<div className="wideDiv">
+				<a href="#">Link 1</a>
+				<a href="#">Link 2</a>
+				<a href="#">Link 3</a>
+			</div>
+		</div>
+	</nav>
+);
 
 // 	burgerToggle: function() {
 // 		let linksEl = document.querySelector('.narrowLinks');
@@ -22,4 +22,4 @@ const NavComponent = () =>
 // 	}
 // });
 
-export default NavComponent
+export default NavComponent;
